072c28534890ab085a20bca8792d4c33e9cdbd82
unknown <sharonlf@vt.edu>
2014-05-06 22:29:55 -0400

    Sharon: made multiple puzzles all kept in one file

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+
+using std::ifstream;
+using std::string;
+using std::getline;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	string the_story;
+	string word_type;
+	string usrNum;
+	string puzzle;
+	int usr;
+	int puz;
+
+	cout << "Welcome to FunLibs!" << endl << "Choose puzzle (#1-3)" << endl;
+	cin >> usrNum;
+	ifstream in("test.txt");
+		
+	getline(in, puzzle, ']');
+
+	while (usrNum != puzzle)
+	{
+		in.ignore (200, '\n');
+		getline(in, puzzle, ']');
+		puz = atoi( puzzle.c_str() );
+	}
+
+	getline(in, the_story, '(');
+	string word;
+
+	while(!in.fail())
+	{
+		getline(in, word_type, ')');
+		if(word_type == "end")
+			break;
+		cout << "Type a " << word_type << endl;
+		cin >> word;
+		the_story.append(word);
+		string appended;
+		getline(in, appended, '(');
+		the_story.append(appended);
+	}
+
+	cout << the_story << endl;
+	cin >> usrNum;
+	return 0;
+}
