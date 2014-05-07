fe9cc99d2a9a7df2487f5249ab37960a8f4be600
Brad <rbrad2015@gmail.com>
2014-05-07 15:13:26 -0400

    added complete another puzzle functionality

--- main.cpp
+++ main.cpp
@@ -21,68 +21,84 @@ int main()
 	string usrNum;
 	string puzzle;
 
-
-	cout << "Welcome to FunLibs!" << endl << "Choose puzzle (#1-6)" << endl;
-	cin >> usrNum;
-	ifstream in("test.txt");
-
-	getline(in, puzzle, ']');
-
-	while (usrNum != puzzle)
-	{
-		in.ignore (200, '\n');
-		getline(in, puzzle, ']');
-	}
-
-	getline(in, the_story, '(');
-	string word;
-
-	while(!in.eof())
-	{
-		getline(in, word_type, ')');
-		if(word_type == "end")
-			break;
-		cout << "Type a " << word_type << endl;
-		cin >> word;
-		the_story.append(word);
-		string appended;
-		getline(in, appended, '(');
-		the_story.append(appended);
-	}
-
-	cout << the_story << endl;
-	
 	while(1){
-		cout << "Would you like to save this story (y/n)" << endl;
+		cout << "Welcome to FunLibs!" << endl << "Choose puzzle (#1-6)" << endl;
+		cin >> usrNum;
+		ifstream in("test.txt");
+
+		getline(in, puzzle, ']');
+
+		while (usrNum != puzzle)
+		{
+			in.ignore (200, '\n');
+			getline(in, puzzle, ']');
+		}
+
+		getline(in, the_story, '(');
+		string word;
+
+		while(!in.eof())
+		{
+			getline(in, word_type, ')');
+			if(word_type == "end")
+				break;
+			cout << "Type a " << word_type << endl;
+			cin >> word;
+			the_story.append(word);
+			string appended;
+			getline(in, appended, '(');
+			the_story.append(appended);
+		}
+
+		cout << the_story << endl;
 		
-		cin >> response;
-		
-		if(response == "yes" || response == "y"){
-			cout << "Please name the output file ending with .txt (default is output.txt)" << endl;
+		while(1){
+			cout << "Would you like to save this puzzle? (y/n)" << endl;
+			
 			cin >> response;
-			if(response != "" && response.substr(response.size() - 4) == ".txt"){
-				outfile = response;
+			
+			if(response == "yes" || response == "y"){
+				cout << "Please name the output file ending with .txt (default is output.txt)" << endl;
+				cin >> response;
+				if(response != "" && response.substr(response.size() - 4) == ".txt"){
+					outfile = response;
+				}
+				else if(response != "" && response.substr(response.size() - 4) != ".txt"){
+					cout << "File name does not end with .txt" << endl;
+					continue;
+				}
+				
+				ofstream out(outfile.c_str());
+				
+				out << the_story << endl;
+				
+				out.close();
+				break;
+				
 			}
-			else if(response != "" && response.substr(response.size() - 4) != ".txt"){
-				cout << "File name does not end with .txt" << endl;
-				continue;
+			else if(response == "no" || response == "n"){
+				break;
 			}
-			
-			ofstream out(outfile.c_str());
-			
-			out << the_story << endl;
-			
-			out.close();
-			break;
-			
+			else{
+				cout << "Response not understood please try again" << endl;
+			}
 		}
-		else if(response == "no" || response == "n"){
-			break;
-		}
-		else{
-			cout << "Response not understood please try again" << endl;
+		
+		while(1){
+			cout << "Would you like to do another puzzle? (y/n)" << endl;
+			
+			cin >> response;
+			
+			if(response == "yes" || response == "y"){
+				break;
+			}
+			else if(response == "no" || response == "n"){
+				return 0;
+			}
+			else{
+				cout << "Response not understood please try again" << endl;
+			}
 		}
 	}
-		
 	return 0;
 }
