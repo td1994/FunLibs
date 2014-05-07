4496136a3b8053c9e10b725bf2a090f989ec02f2
Brad <rbrad2015@gmail.com>
2014-05-07 15:02:25 -0400

    added save story functionality and fixed repeating last input bug'

--- main.cpp
+++ main.cpp
@@ -2,23 +2,28 @@
 #include <string>
 #include <iostream>
 
-using std::ifstream;
-using std::string;
-using std::getline;
-using std::cout;
-using std::cin;
-using std::endl;
+
+// using std::ifstream;
+// using std::ofstream;
+// using std::string;
+// using std::getline;
+// using std::cout;
+// using std::cin;
+// using std::endl;
+using namespace std;
 
 int main()
 {
 	string the_story;
 	string word_type;
+	string response;
+	string outfile = "output.txt";
 
 	ifstream in("test.txt");
 	getline(in, the_story, '(');
 	string word;
 
-	while(!in.fail())
+	while(!in.eof())
 	{
 		getline(in, word_type, ')');
 		if(word_type == "end")
@@ -32,5 +37,40 @@ int main()
 	}
 
 	cout << the_story << endl;
+	
+	
+	while(1){
+		cout << "Would you like to save this story (y/n)" << endl;
+		
+		cin >> response;
+		
+		if(response == "yes" || response == "y"){
+			cout << "Please name the output file ending with .txt (default is output.txt)" << endl;
+			cin >> response;
+			if(response != "" && response.substr(response.size() - 4) == ".txt"){
+				outfile = response;
+			}
+			else if(response != "" && response.substr(response.size() - 4) != ".txt"){
+				cout << "File name does not end with .txt" << endl;
+				continue;
+			}
+			
+			ofstream out(outfile.c_str());
+			
+			out << the_story << endl;
+			
+			out.close();
+			break;
+			
+		}
+		else if(response == "no" || response == "n"){
+			break;
+		}
+		else{
+			cout << "Response not understood please try again" << endl;
+		}
+	}
+		
+	
 	return 0;
 }
